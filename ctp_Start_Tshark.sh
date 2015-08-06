a073041fb164fcf1ca5d30ed0c31636af99ceec2
chaocipher <chaocipher@gmail.com>
2015-08-06 14:14:16 -0700

    Tunning for 250mb files

--- ctp_Start_Tshark.sh
+++ ctp_Start_Tshark.sh
@@ -1,3 +1,3 @@
  
 #This starts tshark against the wifi interface, creating files capped at 100kb in size, and stops capturing after 10 files.
-tshark -i wlo1 -a filesize:100 -a files:10 -w capture
+tshark -i enp0s25 -a filesize:250000 -a files:800 -w capture
